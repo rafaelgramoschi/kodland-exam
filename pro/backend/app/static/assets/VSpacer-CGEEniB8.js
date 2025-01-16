62e09a436c896f9927536e7dd0ba4785009bb394
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-16 15:37:38 +0100

    Add frontend

--- pro/backend/app/static/assets/VSpacer-CGEEniB8.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./forwardRefs-Vhkun_rj.js";const r=a("v-spacer","div","VSpacer");export{r as V};
---
+++ pro/backend/app/static/assets/VSpacer-CTuiujhr.js
@@ -0,0 +1,1 @@
+import{B as a}from"./forwardRefs-0WZftRR9.js";const r=a("v-spacer","div","VSpacer");export{r as V};
