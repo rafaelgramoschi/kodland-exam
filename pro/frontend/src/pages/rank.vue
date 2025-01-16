e89191482e27520965690c7e96a71d151210c71c
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-16 16:55:20 +0100

    Show mock rank table

--- pro/frontend/src/pages/rank.vue
+++ pro/frontend/src/pages/rank.vue
@@ -1,7 +1,17 @@
+<script setup>
+
+const ranks = [
+  { id: 1, username: 'rafa' , score: 100 },
+  { id: 2, username: 'mario' , score: 50 },
+  { id: 3, username: 'edo' , score: 20 },
+]
+</script>
+
 <template>
-rank
+  <v-container>
+    <v-data-table :items="ranks">
+
+    </v-data-table>
+  </v-container>
 </template>
 
-<script setup>
-  //
-</script>
