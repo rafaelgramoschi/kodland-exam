e2083a9956338ed8786af1a6785abae2fa52b0ad
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-16 13:27:59 +0100

    Test fix for prod

---
+++ pro/backend/app/static/assets/VSpacer-CGEEniB8.js
@@ -0,0 +1,1 @@
+import{B as a}from"./forwardRefs-Vhkun_rj.js";const r=a("v-spacer","div","VSpacer");export{r as V};
--- pro/backend/app/static/assets/VSpacer-CNkRJRCR.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./forwardRefs-BsK78O3A.js";const r=a("v-spacer","div","VSpacer");export{r as V};
