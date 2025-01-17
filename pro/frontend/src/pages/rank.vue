b35fa869a36fe83eba4b24828815b09c93bf7cce
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-17 10:46:39 +0100

    Bugfix

--- pro/frontend/src/pages/rank.vue
+++ pro/frontend/src/pages/rank.vue
@@ -6,8 +6,10 @@ const ranks = ref([]);
 
 onMounted(async()=> {
   const response = await fetch(`/ranks`);
+  console.log("RESPONSE ranks:", response)
   const result = await response.json();
-  ranks.value = result?.data?.ranks;
+  console.log("JSON: ", result)
+  ranks.value = result?.ranks;
 });
 
 </script>
