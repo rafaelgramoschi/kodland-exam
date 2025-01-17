c628ae2f8270b351d277952e1cf4a0e64a8df586
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-17 10:03:21 +0100

    Add ranks route

--- pro/backend/app/routes.py
+++ pro/backend/app/routes.py
@@ -4,6 +4,10 @@
 from flask import Flask, render_template
 import db, auth #prod
 #from . import db, auth #dev
+from flask import (
+    jsonify, render_template, request, session,
+    make_response
+)
 
 app = Flask(__name__)
 app.secret_key = 'dev'
@@ -16,4 +20,20 @@ app.register_blueprint(auth.bp)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/ranks', methods=['GET'])
+def ranks():
+    if request.method == 'GET':
+        db = get_db()
+        users = db.execute(
+            'SELECT username, score FROM user'
+        ).fetchall()
+
+        users = [ {"username": user["username"], "score": user["score"]} for user in users ]
+
+        if users:
+            return make_response(jsonify({ "response": "success", "ranks": users }), 200)
+
+    return make_response(jsonify({ "response": "Empty" }), 400)
 
