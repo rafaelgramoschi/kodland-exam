aea3064eec4399c321ac6d2d2f4519b52c7b0685
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-17 10:55:12 +0100

    Fix read

--- pro/frontend/src/stores/user.js
+++ pro/frontend/src/stores/user.js
@@ -6,11 +6,11 @@ import { reactive } from 'vue'
 export const useUserStore = defineStore('user', () => {
   const session = reactive({
     // id: 1,
-    id: localStorage.getItem('session_id'),
-    score: localStorage.getItem('score'),
+    id: parseInt(localStorage.getItem('session_id')) || 0,
+    score: parseInt(localStorage.getItem('score')) || 0,
     reset: () => {
-      localStorage.setItem('session_id', '')
-      localStorage.setItem('score', '')
+      localStorage.removeItem('session_id')
+      localStorage.removeItem('score')
       session.id = null;
       session.score = null;
       router.push({ path: '/login' });
