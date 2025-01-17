23b6acdc5285ff75751e5fd10daf728e5a2586d1
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2025-01-17 10:56:19 +0100

    Update frontend

--- pro/backend/app/static/assets/VSpacer-D-GeEwRj.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./VGrid-BnMF71Bb.js";const r=a("v-spacer","div","VSpacer");export{r as V};
---
+++ pro/backend/app/static/assets/VSpacer-D7TZBwWk.js
@@ -0,0 +1,1 @@
+import{w as a}from"./VGrid-LbbGw_bb.js";const r=a("v-spacer","div","VSpacer");export{r as V};
